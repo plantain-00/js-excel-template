357a0565298200d445f5b849758d9a036e49a62d
york yao <yaoao12306@outlook.com>
2017-08-07 23:02:29 +0800

    update webpack config; improve scripts

--- clean-scripts.config.js
+++ clean-scripts.config.js
@@ -1,24 +1,28 @@
 module.exports = {
   build: [
     `rimraf dist/`,
-    `tsc -p src/tsconfig.nodejs.json`,
-    `tsc -p src/tsconfig.browser.json`,
-    `rollup --config rollup.config.js`,
-    `tsc -p demo`,
-    `webpack --display-modules --config demo/webpack.config.js`
+    {
+      back: `tsc -p src/tsconfig.nodejs.json`,
+      front: [
+        `tsc -p src/tsconfig.browser.json`,
+        {
+          rollup: `rollup --config rollup.config.js`,
+          demo: [
+            `tsc -p demo`,
+            `webpack --display-modules --config demo/webpack.config.js`
+          ]
+        }
+      ]
+    }
   ],
-  lint: [
-    `tslint "*.ts" "spec/*.ts"`,
-    `standard "**/*.config.js"`
-  ],
+  lint: {
+    ts: `tslint "*.ts" "spec/*.ts"`,
+    js: `standard "**/*.config.js"`
+  },
   test: [
     'tsc -p spec',
     'jasmine'
   ],
-  fix: [
-    `standard --fix "**/*.config.js"`
-  ],
-  release: [
-    `clean-release`
-  ]
+  fix: `standard --fix "**/*.config.js"`,
+  release: `clean-release`
 }
