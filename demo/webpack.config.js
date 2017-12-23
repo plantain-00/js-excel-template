b7074dba8fe72c99eaae51272013dd0356a8bff8
york yao <yaoao12306@outlook.com>
2017-12-23 11:55:31 +0800

    feat: update dependencies

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -1,27 +1,5 @@
 const webpack = require('webpack')
 
-const plugins = [
-  new webpack.DefinePlugin({
-    'process.env': {
-      'NODE_ENV': JSON.stringify('production')
-    }
-  }),
-  new webpack.NoEmitOnErrorsPlugin(),
-  new webpack.optimize.UglifyJsPlugin({
-    output: {
-      comments: false
-    },
-    exclude: [
-    ]
-  })
-]
-
-const resolve = {
-  alias: {
-    'vue$': 'vue/dist/vue.esm.js'
-  }
-}
-
 module.exports = [
   {
     entry: './demo/index',
@@ -29,7 +7,20 @@ module.exports = [
       path: __dirname,
       filename: 'index.bundle.js'
     },
-    plugins,
-    resolve
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env': {
+          'NODE_ENV': JSON.stringify('production')
+        }
+      }),
+      new webpack.NoEmitOnErrorsPlugin(),
+      new webpack.optimize.UglifyJsPlugin({
+        output: {
+          comments: false
+        },
+        exclude: [
+        ]
+      })
+    ]
   }
 ]
