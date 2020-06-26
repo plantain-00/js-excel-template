4cf529c2883788a4588fe5ce104d9dabaee2e907
York Yao <me@yorkyao.com>
2020-06-26 10:33:43 +0800

    chore: remove commitlint

--- clean-scripts.config.ts
+++ clean-scripts.config.ts
@@ -21,7 +21,6 @@ export default {
   lint: {
     ts: `eslint --ext .js,.ts,.tsx ${tsFiles} ${jsFiles}`,
     export: `no-unused-export ${tsFiles}`,
-    commit: `commitlint --from=HEAD~1`,
     markdown: `markdownlint README.md`,
     typeCoverage: 'type-coverage -p src/tsconfig.nodejs.json --strict',
     typeCoverageBrowser: 'type-coverage -p src/tsconfig.browser.json --strict'
